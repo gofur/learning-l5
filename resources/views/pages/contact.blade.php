d20a69927db478f2f27bba746a83b8c903d1a6b0
Abdul Ghapur <gofurbrente01@gmail.com>
2015-08-04 16:56:43 +0700

    Learning laravel 5.1

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Contact</title>
+        <!-- <link rel="stylesheet" href="css/style.css">
+        <link rel="author" href="humans.txt"> -->
+    </head>
+    <body>
+       
+       <h1>Contact Me!</h1> 
+       
+    </body>
+</html>
